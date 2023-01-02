7c6deeee2c5777d9131701ad32985a25d5d77a38
tanay <iamtanaylodh@gmail.com>
2023-01-02 17:54:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				biege: {
+					1: "#FFF8E1",
+					2: "#FAEAB1",
+					3: "#E5BA73",
+					4: "#C58940",
+				},
+				brown: {
+					1: "#E6B325",
+					2: "#BF9742",
+					3: "#A47E3B",
+					4: "#61481C",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
